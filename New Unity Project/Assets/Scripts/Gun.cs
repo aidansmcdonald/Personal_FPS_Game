462801c9835e4d7d2928990b0cc20a55c63de819
aidansmcdonald <aidans.mcdonald@gmail.com>
2020-10-10 15:00:15 -0400

    Started FPS game - Added player + inputs, shooting and semi and fully automatic firing modes

---
+++ New Unity Project/Assets/Scripts/Gun.cs
@@ -0,0 +1,78 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    //Start location of bullets
+    public Transform launchPosition;
+
+    public float bulletSpeed = 20.0f;
+
+    bool semiAuto = true;
+
+    void fireBullet()
+    {
+        Rigidbody bullet = createBullet();
+        bullet.velocity = transform.parent.forward * bulletSpeed;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check input manager to see if left mouse button is held (1 = right, 2 = middle)
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Makes weapon semi automatic
+            if (semiAuto == true)
+            {
+                Invoke("fireBullet", 0f);
+            }
+
+            if (semiAuto == false)
+            {            
+                //(makes weapon fully automatic)
+                //If fireBullet is not being invoked, call invoke repeating 
+                if (!IsInvoking("fireBullet"))
+                {
+                    InvokeRepeating("fireBullet", 0f, 0.1f);
+                }
+            }
+        }
+        //Check if mouse button is released, cancel shooting
+        if (Input.GetMouseButtonUp(0))
+        {
+            CancelInvoke("fireBullet");
+        }
+    }
+
+    private Rigidbody createBullet()
+    {
+        // Instantiate creates a prefab (bullet)
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        // Sets bullet position to launch position (gun barrel)
+        bullet.transform.position = launchPosition.position;
+        return bullet.GetComponent<Rigidbody>();
+    }
+
+    public void ChangeWeapon()
+    {
+        if (semiAuto == true)
+        {
+            semiAuto = false;
+        }
+        else
+        {
+            semiAuto = true;
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 50.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get current position of game object (player)
+        Vector3 pos = transform.position;
+
+        //speed in x direction is increased by Input.GetAxis("Horizontal") over time, 
+        //where horizontal is 'name' in project settings input
+        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        //Above code for vertical axis
+        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+
+        //Sets game object's (player) position to the new position
+        transform.position = pos;
+
+    }
+}
